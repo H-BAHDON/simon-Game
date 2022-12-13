adddcdce913144f997d8b2d197a1d71a2dabc56f
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-12-13 14:24:25 +0000

    updated

--- game.js
+++ game.js
@@ -1,15 +1,18 @@
-    var buttonColours = ["red", "blue", "green", "yellow"];
+    buttonColours = ["red", "blue", "green", "yellow"];
+    gamePattern = [];
+    userClickedPattern = [];
 
-    var userClickedPattern = [];
-
-    var gamePattern = []
+    let level = 0;
+    let started = false;
 
     function nextSequence() {
-      var randomNumber = Math.floor(Math.random() * 4);
-      var randomChosenColor = buttonColours[randomNumber];
-      $()
+      level++;
+      let randomNumber = Math.random();
+      randomNumber = Math.floor(randomNumber * 4);
+      let randomChosenColor = buttonColours[randomNumber];
       gamePattern.push(randomChosenColor);
       playSound(randomChosenColor);
+      animatePress(randomNumber);
     }
 
     function playSound(name) {
@@ -18,22 +21,36 @@
     }
 
 
-    $(".btn").on("click", function() {
+    $(".btn").on("click", function(){
       var userChosenColour = $(this).attr("id");
-      userClickedPattern.push(userChosenColour)
+      userClickedPattern.push(userChosenColour);
       playSound(userChosenColour);
       $(this).fadeIn(100).fadeOut(100).fadeIn(100);
       console.log(userClickedPattern);
-      animate(userChosenColour);
-    });
+      animatePress(userChosenColour);
+    })
 
 
-    function animate(currentColor) {
+    function animatePress(currentColor){
       $("#" + currentColor).addClass("pressed");
-      setTimeout(function() {
+      setTimeout(function(){
         $("#" + currentColor).removeClass("pressed")
       }, 100)
-    };
+    }
+
+
+
+    $(document).on("keypress", () =>{
+      if(!started){
+          $("#level-title").text("level " + level++);
+          nextSequence();
+          started = true;
+      }
+    })
+
+    function checkAnswer(currentlevel){
+      
+    }
 
 
 
@@ -42,5 +59,6 @@
 
 
 
+// this.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    //this is the end of the Java-script
+    //the end of the js file
